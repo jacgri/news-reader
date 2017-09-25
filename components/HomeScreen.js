943aac416c26ac77165db1dddfbea6419858708b
Jacqui Grimes <grimesjacqui@gmail.com>
2017-09-25 20:28:43 +0100

    returning Guardian news through promise

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,7 +1,46 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import API_KEY from '../key'
+import axios from 'axios'
 
 class HomeScreen extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      news: []
+    }
+    this.handleNewsUpdate = this.handleNewsUpdate.bind(this)
+  }
+
+  handleNewsUpdate (response) {
+    const results = response.data.response.results
+    const news = []
+
+    results.forEach(function (result) {
+      const existingSection = news.find(function getBySectionName (section) {
+        return section.title === result.sectionName
+      })
+      if (existingSection) {
+        existingSection.data.push(result)
+      } else {
+        news.push({
+          title: result.sectionName,
+          data: [result]
+        })
+      }
+    })
+    this.setState({ news })
+  }
+
+  componentDidMount () {
+    axios.get(`https://content.guardianapis.com/search?api-key=${API_KEY}`)
+    .then(response => {
+      this.handleNewsUpdate(response)
+    })
+    .catch(error => console.log(error))
+  }
+
   render () {
     return (
       <View>
---
+++ key.js
@@ -0,0 +1,3 @@
+const API_KEY = '68bdcdf8-dbb0-48a7-b044-3b9452419885'
+
+export default API_KEY
